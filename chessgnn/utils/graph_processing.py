e63f1cf980d21af26bd471b0f630f1b2ab0001bf
marcinz99 <marcinzieba1999@gmail.com>
2023-01-14 20:11:03 +0100

    implemented and tested get neighbors with kdtree function

--- chessgnn/utils/graph_processing.py
+++ chessgnn/utils/graph_processing.py
@@ -1,10 +1,11 @@
 import numpy as np
-from typing import List, Optional, Tuple
+from sklearn.neighbors import KDTree
+from typing import List, Optional, Tuple, Union
 
 
 def get_adjacency_matrix_from_edge_list(
         n: int,
-        edges: Optional[List[Tuple[int, int]]] = None,
+        edges: Optional[Union[List[Tuple[int, int]], np.ndarray]] = None,
         directed: bool = False,
         self_connections: bool = False,
 ) -> np.ndarray:
@@ -13,18 +14,19 @@ def get_adjacency_matrix_from_edge_list(
 
     Args:
         n: Number of vertices.
-        edges: List of tuples defining the vertices connected by the edges - (v_from, v_to) if directed.
+        edges: List of tuples defining the vertices connected by the edges - (v_from, v_to) if directed. Alternatively,
+            2D array of shape [n, 2] with edges in rows can also be passed.
         directed: If graph is undirected, opposite edges will also be included.
         self_connections: Whether to include the self-connections of vertices.
 
     Returns:
         Two-dimensional array with integer zeros (no connection) and ones (connection).
     """
-    # Initialize adjacency matrix with zeros
+    # Initialize adjacency matrix with zeros.
     adj = np.zeros([n, n], dtype=np.int32)
 
-    # Process the edges
-    edges = np.array(edges if edges else [], dtype=np.int32).reshape(-1, 2)
+    # Process the edges.
+    edges = np.array(edges if (isinstance(edges, np.ndarray) or edges) else [], dtype=np.int32).reshape(-1, 2)
 
     if edges.shape[0] > 0:
         assert np.min(edges) >= 0 and np.max(edges) < n, "Edges out of range"
@@ -46,3 +48,88 @@ def get_adjacency_matrix_from_edge_list(
     adj = np.minimum(adj, 1)
 
     return adj
+
+
+def get_neighbors_graph_with_kdtree(
+        X: np.ndarray,
+        kdtree: KDTree,
+        radius: Optional[Union[int, float]] = None,
+        limit: Optional[int] = None,
+        randomized: bool = False,
+) -> List[np.ndarray]:
+    """
+    Constructs a neighboring graphs using a KD-Tree in a batch setting.
+
+    KD-Tree is used specifically for faster processing of large inputs (high number of vertices).
+
+    Args:
+        X: Query points organized in a 3D or 2D array. If 2D array is passed, each row is a single point of a given
+            dimensionality; in case of 3D input, the outermost dimension is for batch.
+        kdtree: KDTree with the points to be extracted.
+        radius: Maximum interaction distance for neighbors.
+        limit: Maximum number of neighbors per vertex.
+        randomized: If both limit and radius are set, you may want to randomize the results before limiting. Otherwise,
+            the results are returned sorted.
+
+    Returns:
+        A list of graphs in COOrdinate format, organized as a 2D array, where first column corresponds to the edge
+        sources (from X) and the second one to the edge sinks (from KD-Tree). Keep in mind, however, that the same index
+        in X and KD-Tree MAY NOT correspond to the same point, unless you made it so.
+    """
+    assert radius or limit, "Either `radius` or `limit` has to be set."
+
+    # If there is a single query matrix, parse it as a single-example batch anyway.
+    if len(X.shape) == 2:
+        X = X[np.newaxis, ...]
+
+    def shuffle(x: np.ndarray) -> np.ndarray:
+        # Supplementary function for inline shuffle. Maybe a better solution can be found.
+        np.random.shuffle(x)
+        return x
+
+    # Initialize the (yet empty) list of graphs in COO format.
+    coo_graphs = []
+
+    # Prepare proper mapping for parsing the neighbors pairs.
+    # * Pair the source vertex (x[0]: int) with the neighbors (x[1]: array 1D).
+    # * Parse as 2D array of shape [_, 2].
+    if randomized:
+        # Shuffle and limit the results, if exceeding.
+        def mapping(x: Tuple[int, np.ndarray]) -> np.ndarray:
+            return shuffle(
+                np.pad(x[1].reshape(-1, 1), pad_width=[(0, 0), (1, 0)], constant_values=x[0])
+            )[:limit]
+    else:
+        # Just limit the results, if exceeding.
+        def mapping(x: Tuple[int, np.ndarray]) -> np.ndarray:
+            return np.pad(x[1].reshape(-1, 1), pad_width=[(0, 0), (1, 0)], constant_values=x[0])[:limit]
+
+    # Prepare a proper neighbor retrieval method.
+    if radius:
+        # Search inside a given radius.
+        def obtain_neighbors(X_in_batch: np.ndarray) -> np.ndarray:
+            # Note: In query_radius `sort_results=True` requires `return_distance=True`.
+            return kdtree.query_radius(X_in_batch, r=radius, sort_results=True, return_distance=True)[0]
+    else:
+        # Unconstrained search.
+        def obtain_neighbors(X_in_batch: np.ndarray) -> np.ndarray:
+            return kdtree.query(X_in_batch, k=limit, sort_results=True, return_distance=False)
+
+    for X_in_batch in X:
+        # Repeat over each element of the batch.
+
+        neighbors = list(map(
+            # Mapping prepared earlier.
+            mapping,
+
+            # Enumerate over the neighbors returned by KD-Tree query.
+            enumerate(obtain_neighbors(X_in_batch)),
+        ))
+
+        # Flatten the neighbors list to a single array of shape [_, 2].
+        neighbors = np.concatenate(neighbors, axis=0)
+
+        # Append the COO representation to the list.
+        coo_graphs.append(neighbors)
+
+    return coo_graphs

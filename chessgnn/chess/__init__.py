be1793c25b7a5af0c42e3670fe7b26054870ff2e
marcinz99 <marcinzieba1999@gmail.com>
2023-05-08 17:32:15 +0200

    insert data preparation code

--- chessgnn/chess/__init__.py
+++ chessgnn/chess/__init__.py
@@ -1,4 +1,5 @@
 __all__ = [
     '.translations',
     '.connectivity',
+    '.data_preparation',
 ]
--- chessgnn/chess/data_preparation.py
+++ chessgnn/chess/data_preparation.py
@@ -1,7 +1,7 @@
 import chess
 import numpy as np
 import pandas as pd
-from chessgnn.chess.translations import field_to_int
+from chessgnn.chess.translations import field_to_int, piece_placement_from_fen_code
 from typing import List, Tuple
 
 
@@ -105,3 +105,203 @@ def parse_and_replay(games_df: pd.DataFrame, n_from: int, n_to: int) -> List[Tup
 
     print()
     return states_all
+
+
+def states_all_to_moves_dataframe(states_all: List[Tuple]) -> pd.DataFrame:
+    moves = pd.DataFrame(states_all, columns=[
+        "game_index", "total_len", "final_result", "latest_move_san", "latest_move_uci", "board_fen",
+        "possible_moves", "possible_captures"])
+    moves['latest_move'] = moves['latest_move_uci'].apply(lambda x: move_list_encode([x]))
+
+    moves = pd.concat([
+        moves.drop("board_fen", axis=1),
+        pd.DataFrame(
+            moves['board_fen'].apply(lambda x: x.split(' ')).tolist(),
+            columns=["piece_placement", "who_moves_next", "castling_rights", "en_passant_targets",
+                     "halfmove_clock", "fullmove_number"]
+        )
+    ], axis=1)
+    moves['halfmove_clock'] = moves['halfmove_clock'].astype(np.int32)
+    moves['fullmove_number'] = moves['fullmove_number'].astype(np.int32)
+
+    moves['ply_counter'] = (moves.groupby(['game_index']).cumcount() + 1).astype(np.int32)
+    moves['state_repetition'] = moves.groupby(
+        ['game_index', "piece_placement", "castling_rights", "en_passant_targets"]).cumcount()
+
+    moves['piece_placement'] = moves['piece_placement'].apply(piece_placement_from_fen_code)
+    moves['lm_capture'] = moves['latest_move_san'].apply(lambda x: 'x' in x)
+    moves['lm_check'] = moves['latest_move_san'].apply(lambda x: '+' in x)
+    moves['lm_mate'] = moves['latest_move_san'].apply(lambda x: '#' in x)
+    moves['lm_kingside_castling'] = moves['latest_move_san'].apply(lambda x: "O-O" in x)
+    moves['lm_queenside_castling'] = moves['latest_move_san'].apply(lambda x: "O-O-O" in x)
+    moves['lm_kingside_castling'] = (moves['lm_kingside_castling'] & (~moves['lm_queenside_castling']))
+    moves['lm_promotion'] = moves['latest_move_san'].apply(lambda x: '=' in x)
+
+    moves['en_passant_target'] = moves['en_passant_targets'].apply(lambda x: field_to_int[x])
+    moves['K_castling_ability'] = moves['castling_rights'].apply(lambda x: 'K' in x)
+    moves['Q_castling_ability'] = moves['castling_rights'].apply(lambda x: 'Q' in x)
+    moves['k_castling_ability'] = moves['castling_rights'].apply(lambda x: 'k' in x)
+    moves['q_castling_ability'] = moves['castling_rights'].apply(lambda x: 'q' in x)
+
+    moves = moves.drop(["castling_rights", "en_passant_targets"], axis=1)
+
+    moves['place_from'] = moves['latest_move_uci'].apply(lambda x: field_to_int[x[0:2]])
+    moves['place_to'] = moves['latest_move_uci'].apply(lambda x: field_to_int[x[2:4]])
+    moves['promoted_to'] = moves['latest_move_uci'].apply(lambda x: x[4:])
+    moves['lm_piece_type'] = (
+        moves[['piece_placement', 'place_to']]
+        .apply(lambda x: (x['piece_placement'][x['place_to'] - 1] - 1) % 6 + 1, axis='columns'))
+
+    moves['en_passant_target_prev'] = np.where(
+        moves['game_index'].shift(1, fill_value=-1) == moves['game_index'],
+        moves['en_passant_target'].shift(1, fill_value=0),
+        0).reshape(-1, 1)
+    moves['lm_en_passant'] = ((moves['en_passant_target_prev'] == moves['place_to']) & (moves['lm_piece_type'] == 6))
+
+    return moves
+
+
+def calculate_tracking(moves: pd.DataFrame) -> pd.DataFrame:
+    latest_idx = -1
+
+    pos_list = []
+    typ_list = []
+    moved_list = []
+    captured_list = []
+
+    for i, (idx, uci, color, kingside, queenside, en_passant, new_typing) in moves[
+        ['game_index', 'latest_move_uci', 'who_moves_next', 'lm_kingside_castling', 'lm_queenside_castling',
+         'lm_en_passant', 'lm_piece_type']
+    ].iterrows():
+        print(f"\rGame: {idx + 1}", end='')
+        if idx != latest_idx:
+            positions = starting_positions.copy()
+            typings = starting_typing.copy()
+        latest_idx = idx
+
+        moved, captured = apply_move(
+            positions, typings, uci, kingside or queenside, color == 'b', kingside, en_passant, new_typing)
+        pos_list += [positions.copy().astype(np.uint8)]
+        typ_list += [typings.copy().astype(np.uint8)]
+        moved_list += [moved]
+        captured_list += [captured]
+
+    moves['piece_tracking'] = pd.Series(pos_list)
+    moves['piece_typing'] = pd.Series(typ_list)
+    moves['lm_idx_moved'] = pd.Series(moved_list)
+    moves['lm_idx_captured'] = pd.Series(captured_list)
+
+    moves = moves.astype(numeric_types)
+
+    print()
+    return moves
+
+
+def concat_initial_position(moves: pd.DataFrame) -> pd.DataFrame:
+    board = chess.Board()
+    initial_pos_encoding = pd.DataFrame([{
+        'latest_move_san': '--',
+        'latest_move_uci': '----',
+        'possible_moves': move_list_encode(board.generate_legal_moves()),
+        'possible_captures': move_list_encode(board.generate_legal_captures()),
+        'latest_move': np.array([], dtype=np.uint8),
+        'piece_placement': piece_placement_from_fen_code(board.board_fen()),
+        'who_moves_next': 'w',
+        'halfmove_clock': 0,
+        'fullmove_number': 1,
+        'ply_counter': 0,
+        'state_repetition': 0,
+        'lm_capture': False,
+        'lm_check': False,
+        'lm_mate': False,
+        'lm_kingside_castling': False,
+        'lm_queenside_castling': False,
+        'lm_promotion': False,
+        'en_passant_target': 0,
+        'K_castling_ability': True,
+        'Q_castling_ability': True,
+        'k_castling_ability': True,
+        'q_castling_ability': True,
+        'place_from': 0,
+        'place_to': 0,
+        'promoted_to': '',
+        'lm_piece_type':  0,
+        'en_passant_target_prev': 0,
+        'lm_en_passant': False,
+        'piece_tracking': starting_positions,
+        'piece_typing': starting_typing,
+        'lm_idx_moved': np.array([], dtype=np.uint8),
+        'lm_idx_captured': np.array([], dtype=np.uint8),
+    }]).astype({k: v for k, v in numeric_types.items() if k not in ['game_index', 'total_len', 'final_result']})
+
+    initial_states = (
+        moves[['game_index', 'total_len', 'final_result']]
+        .drop_duplicates()
+        .merge(initial_pos_encoding, how='cross')
+    )
+
+    moves = (
+        pd.concat([moves, initial_states], axis='rows')
+        .sort_values(['game_index', 'ply_counter'])
+        .reset_index(drop=True)
+    )
+
+    moves['plies_till_end'] = moves['total_len'] - moves['ply_counter']
+
+    return moves
+
+
+def concat_shifted_features(moves: pd.DataFrame) -> pd.DataFrame:
+    def shifted_col(game_index_col, target_col, n_shift):
+        return pd.Series(np.where(
+            game_index_col == game_index_col.shift(-n_shift),
+            target_col.shift(-n_shift),
+            pd.Series(list(np.zeros([moves.shape[0], 0], dtype=np.uint8)))
+        ))
+
+    def future_checker(game_index_col, target_col, n_shift):
+        return (game_index_col == game_index_col.shift(-n_shift)) & target_col.shift(-n_shift)
+
+    moves['future_idx_moved'] = (
+        pd.DataFrame(pd.concat(
+            [shifted_col(moves['game_index'], moves['lm_idx_moved'], n_shift=i) for i in range(1, 11)],
+            axis='columns'))
+        .apply(lambda x: np.concatenate(x), axis='columns')
+    )
+    moves['future_idx_captured'] = (
+        pd.DataFrame(pd.concat(
+            [shifted_col(moves['game_index'], moves['lm_idx_captured'], n_shift=i) for i in range(1, 11)],
+            axis='columns'))
+        .apply(lambda x: np.concatenate(x), axis='columns')
+    )
+    moves['future_move'] = shifted_col(moves['game_index'], moves['latest_move'], n_shift=1)
+
+    checks_in_future = pd.DataFrame(pd.concat(
+        [future_checker(moves['game_index'], moves['lm_check'], n_shift=i) for i in range(1, 11)],
+        axis='columns'))
+
+    moves['future_give_check_in_1'] = checks_in_future[[0]].sum(axis='columns').astype(bool)
+    moves['future_give_check_in_3'] = checks_in_future[[0, 2, 4]].sum(axis='columns').astype(bool)
+    moves['future_give_check_in_5'] = checks_in_future[[0, 2, 4, 6, 8]].sum(axis='columns').astype(bool)
+    moves['future_get_checked_in_1'] = checks_in_future[[1]].sum(axis='columns').astype(bool)
+    moves['future_get_checked_in_3'] = checks_in_future[[1, 3, 5]].sum(axis='columns').astype(bool)
+    moves['future_get_checked_in_5'] = checks_in_future[[1, 3, 5, 7, 9]].sum(axis='columns').astype(bool)
+
+    return moves
+
+
+def generate_boards_daraframe(games_df: pd.DataFrame, n_from: int, n_to: int) -> pd.DataFrame:
+    print(f"[{n_from}-{n_to}] Generating board features")
+    print("> Parsings plays and replaying")
+    states_all = parse_and_replay(games_df, n_from, n_to)
+    print("> Processing gather metadata into dataframe")
+    moves = states_all_to_moves_dataframe(states_all)
+    print("> Tracking pieces")
+    moves = calculate_tracking(moves)
+    print("> Appending initial position")
+    moves = concat_initial_position(moves)
+    print("> Appending shifted features")
+    moves = concat_shifted_features(moves)
+    print("> Done!")
+
+    return moves
